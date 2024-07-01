a77941135d04ba8e0b548aa3698d704131e065f2
matheus-dev-fullstack <matheus178541@gmail.com>
2024-07-01 19:52:36 -0300

    Adição de testes utilizando interação com a barra de pesquisa, botão de pesquisar e resultados

--- cypress/e2e/ebac-jobs/home.cy.js
+++ cypress/e2e/ebac-jobs/home.cy.js
@@ -3,6 +3,14 @@
 describe('Testes para a home', () => {
     it('Deve renderizar 4 vagas', () => {
         cy.visit('https://ebac-jobs-e2e.vercel.app/')
-        cy.get('.ListaVagas_vagas_gmNzn li').should('have.length', 4)
+        cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 4)
+    })
+
+    it('Deve filtrar por fullstack', () => {
+        cy.visit('https://ebac-jobs-e2e.vercel.app/')
+        cy.get('.FormVagas_campo__E1ppF').type('fullstack{enter}')
+        cy.get('button[type="submit"]').click()
+        cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 1)
+
     })
 })
